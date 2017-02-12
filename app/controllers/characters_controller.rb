6c0adf7e691a191b14095a609b7a024b234d4c9a
luke <luke.p.oconnor@gmail.com>
2017-02-12 10:32:29 -0800

    index and show routes

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,2 +1,11 @@
 class CharactersController < ApplicationController
+  def index
+    @characters = Character.all
+    render "index.json.jbuilder"
+  end
+
+  def show
+    @character = Character.find_by(id: params[:id])
+    render "show.json.jbuilder"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   get "/characters" => "characters#index"
+
+  get "/characters/:id" => "characters#show"
 end
