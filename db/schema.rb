9f8a092af93ae744e81259c7e44a8f8129ca3aac
luke <luke.p.oconnor@gmail.com>
2017-02-14 20:55:58 -0800

    added user model and api authentication

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170212175848) do
+ActiveRecord::Schema.define(version: 20170215044530) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20170212175848) do
     t.string   "name"
     t.string   "house"
     t.string   "city"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "api_key"
+    t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
